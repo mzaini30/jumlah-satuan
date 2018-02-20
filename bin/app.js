28caeb8539d12bd1b5b602feef7a2067727faedb
mzaini30 <muhzaini30@gmail.com>
2018-02-20 00:06:44 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -2,9 +2,9 @@ jQuery(function($){
 	angka = '';
 	$('#angka').keyup(function(){
 		angka = $(this).val();
-		$('.hasil').html(angka);
+		
 		if ($('.hasil').html() == ''){
-			$(this).html('0');
+			$('.hasil').html('0');
 		}
 	});
 });
