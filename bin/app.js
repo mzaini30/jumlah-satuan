5778ccf41ff4774a273075200cc07388ba36301e
mzaini30 <muhzaini30@gmail.com>
2018-02-20 00:09:54 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -1,8 +1,7 @@
 jQuery(function($){
-	angka = '';
 	$('#angka').keyup(function(){
 		angka = $(this).val();
-		
+		pecah = angka.split('');
 		if ($('.hasil').html() == ''){
 			$('.hasil').html('0');
 		}
