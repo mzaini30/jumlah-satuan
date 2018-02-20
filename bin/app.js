c1dcb7294d27ec1d2b9159dabb3fa2447275021f
mzaini30 <muhzaini30@gmail.com>
2018-02-20 00:12:10 +0000

    oke

--- bin/app.js
+++ bin/app.js
@@ -2,6 +2,11 @@ jQuery(function($){
 	$('#angka').keyup(function(){
 		angka = $(this).val();
 		pecah = angka.split('');
+		jumlah = 0;
+		for (n in pecah){
+			jumlah += Number(pecah[n]);
+		}
+		$('.hasil').html(jumlah);
 		if ($('.hasil').html() == ''){
 			$('.hasil').html('0');
 		}
